ef7f0e5d9bf66e4df0a74175b09132f3d2299d8b
Thibault PLET <thibault.plet@gmail.com>
2016-04-10 17:15:39 +0200

    Update data possibility from Packet (declare predefined data type -> max 32 bytes for all data sended by RF24)

--- com/osteres/automation/sensor/Packet.cpp
+++ com/osteres/automation/sensor/Packet.cpp
@@ -1,10 +1,12 @@
 //
 // Created by Thibault PLET <thibault.plet@gmail.com> on 01/04/2016.
 //
+#include <stdint.h>
 #include "Packet.h"
 #include "IDGenerator.h"
+#include <string.h>
 
-typedef unsigned char byte;
+typedef uint8_t byte;
 
 namespace com
 {
@@ -22,7 +24,6 @@ namespace com
                     this->id = 0x0;
                     this->sensor = 0x0;
                     this->date = 0;
-                    this->data = 0;
                 }
 
                 Packet::Packet(byte s, byte i)
@@ -30,7 +31,6 @@ namespace com
                     this->sensor = s;
                     this->id = i;
                     this->date = 0;
-                    this->data = 0;
                 }
 
                 /**
@@ -76,7 +76,7 @@ namespace com
                 /**
                  * Get timestamp associated to data
                  */
-                int Packet::getDate()
+                unsigned long int Packet::getDate()
                 {
                     return this->date;
                 }
@@ -84,26 +84,94 @@ namespace com
                 /**
                  * Set timestamp associated to data
                  */
-                void Packet::setDate(int date)
+                void Packet::setDate(unsigned long int date)
                 {
                     this->date = date;
                 }
 
-                /**
-                 * Get data
-                 */
-                int Packet::getData()
-                {
-                    return this->data;
-                }
+                byte Packet::getDataByte1()
+                { return this->dataByte1; }
 
-                /**
-                 * Set data
-                 */
-                void Packet::setData(int data)
-                {
-                    this->data = data;
-                }
+                byte Packet::getDataByte2()
+                { return this->dataByte2; }
+
+                byte Packet::getDataByte3()
+                { return this->dataByte3; }
+
+                byte Packet::getDataByte4()
+                { return this->dataByte4; }
+
+                byte Packet::getDataByte5()
+                { return this->dataByte5; }
+
+                byte Packet::getDataByte6()
+                { return this->dataByte6; }
+
+                void Packet::setDataByte1(byte data)
+                { this->dataByte1 = data; }
+
+                void Packet::setDataByte2(byte data)
+                { this->dataByte2 = data; }
+
+                void Packet::setDataByte3(byte data)
+                { this->dataByte3 = data; }
+
+                void Packet::setDataByte4(byte data)
+                { this->dataByte4 = data; }
+
+                void Packet::setDataByte5(byte data)
+                { this->dataByte5 = data; }
+
+                void Packet::setDataByte6(byte data)
+                { this->dataByte6 = data; }
+
+                long Packet::getDataLong1()
+                { return this->dataLong1; }
+
+                long Packet::getDataLong2()
+                { return this->dataLong2; }
+
+                long Packet::getDataLong3()
+                { return this->dataLong3; }
+
+                long Packet::getDataLong4()
+                { return this->dataLong4; }
+
+                void Packet::setDataLong1(long data)
+                { this->dataLong1 = data; }
+
+                void Packet::setDataLong2(long data)
+                { this->dataLong2 = data; }
+
+                void Packet::setDataLong3(long data)
+                { this->dataLong3 = data; }
+
+                void Packet::setDataLong4(long data)
+                { this->dataLong4 = data; }
+
+                char Packet::getDataChar1()
+                { return this->dataChart1; }
+
+                char Packet::getDataChar2()
+                { return this->dataChart2; }
+
+                char Packet::getDataChar3()
+                { return this->dataChart3; }
+
+                char Packet::getDataChar4()
+                { return this->dataChart4; }
+
+                void Packet::setDataChar1(char data)
+                { this->dataChart1 = data; }
+
+                void Packet::setDataChar2(char data)
+                { this->dataChart2 = data; }
+
+                void Packet::setDataChar3(char data)
+                { this->dataChart3 = data; }
+
+                void Packet::setDataChar4(char data)
+                { this->dataChart4 = data; }
 
                 /**
                  * Set packet identifier by using next uniq identifier
--- com/osteres/automation/sensor/Packet.h
+++ com/osteres/automation/sensor/Packet.h
@@ -5,7 +5,7 @@
 #ifndef COM_OSTERES_AUTOMATION_SENSOR_PACKET_H
 #define COM_OSTERES_AUTOMATION_SENSOR_PACKET_H
 
-typedef unsigned char byte;
+typedef uint8_t byte;
 
 namespace com
 {
@@ -61,23 +61,46 @@ namespace com
                     /**
                      * Get timestamp associated to data
                      */
-                    int getDate();
+                    unsigned long int getDate();
 
                     /**
                      * Set timestamp associated to data
                      */
-                    void setDate(int date);
+                    void setDate(unsigned long int date);
 
-                    /**
-                     * Get data
-                     */
-                    int getData();
+                    byte getDataByte1();
+                    byte getDataByte2();
+                    byte getDataByte3();
+                    byte getDataByte4();
+                    byte getDataByte5();
+                    byte getDataByte6();
 
-                    /**
-                     * Set data
-                     */
-                    void setData(int data);
+                    void setDataByte1(byte data);
+                    void setDataByte2(byte data);
+                    void setDataByte3(byte data);
+                    void setDataByte4(byte data);
+                    void setDataByte5(byte data);
+                    void setDataByte6(byte data);
 
+                    long getDataLong1();
+                    long getDataLong2();
+                    long getDataLong3();
+                    long getDataLong4();
+
+                    void setDataLong1(long data);
+                    void setDataLong2(long data);
+                    void setDataLong3(long data);
+                    void setDataLong4(long data);
+
+                    char getDataChar1();
+                    char getDataChar2();
+                    char getDataChar3();
+                    char getDataChar4();
+
+                    void setDataChar1(char data);
+                    void setDataChar2(char data);
+                    void setDataChar3(char data);
+                    void setDataChar4(char data);
 
                 protected:
                     /**
@@ -91,14 +114,49 @@ namespace com
                     byte sensor;
 
                     /**
-                     * Timestamp associated to data
+                     * Containers
+                     * Be careful to not exceed 32 bytes on total properties size!
+                     * Actually: 32 bytes (26 bytes for data)
+                     *
+                     * Check var type size between arduino and raspberry before declare it
+                     * Raspbbery unknow: When 30 bytes declared, sizeof(packet) equal to 32... Work with step ?
+                     *                   (4, 8, 16, 32 ?)
+                     *                   To fix, ensure that exactly one of these step defined
                      */
-                    int date;
 
                     /**
-                     * Data sended
+                     * Data byte containers
+                     * 6 x 1 byte = 6 bytes
                      */
-                    int data;
+                    byte dataByte1;
+                    byte dataByte2;
+                    byte dataByte3;
+                    byte dataByte4;
+                    byte dataByte5;
+                    byte dataByte6;
+
+                    /**
+                     * Data long containers
+                     * 4 x 4 bytes = 16 bytes
+                     */
+                    long dataLong1;
+                    long dataLong2;
+                    long dataLong3;
+                    long dataLong4;
+
+                    /**
+                     * Data chart containers
+                     * 4 x 1 byte = 4 bytes
+                     */
+                    char dataChart1;
+                    char dataChart2;
+                    char dataChart3;
+                    char dataChart4;
+
+                    /**
+                     * Timestamp associated to data
+                     */
+                    unsigned long int date;
 
                 };
             }
