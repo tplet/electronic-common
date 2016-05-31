446c41ed1b1fb743c30a3a3aaf509ad0935e87c8
Thibault PLET <thibault.plet@gmail.com>
2016-05-31 23:06:15 +0200

    Add success state to Action base class

--- com/osteres/automation/action/Action.h
+++ com/osteres/automation/action/Action.h
@@ -18,9 +18,66 @@ namespace com
                 {
                 public:
                     /**
-                     * Execute business action
+                     * Constructor
                      */
-                    virtual void execute();
+                    Action()
+                    {
+                        this->success = false;
+                        this->executed = false;
+                    }
+
+                    /**
+                     * Execute business action
+                     * Return success state
+                     */
+                    virtual bool execute() {
+                        this->executed = true;
+
+                        return true;
+                    }
+
+                    /**
+                     * Get success state
+                     */
+                    bool isSuccess()
+                    {
+                        return this->success;
+                    }
+
+                    /**
+                     * Declare action as successfully executed
+                     */
+                    void setSuccess()
+                    {
+                        this->success = true;
+                    }
+
+                    /**
+                     * Declare action as not successfully executed
+                     */
+                    void setError()
+                    {
+                        this->success = false;
+                    }
+
+                    /**
+                     * Get executed flag
+                     */
+                    bool isExecuted()
+                    {
+                        return this->executed;
+                    }
+
+                protected:
+                    /**
+                     * Success flag
+                     */
+                    bool success;
+
+                    /**
+                     * Execution flag
+                     */
+                    bool executed;
                 };
             }
         }
