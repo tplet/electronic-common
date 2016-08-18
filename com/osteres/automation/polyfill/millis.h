d609dddc393cda0a5b80893fef9c476998d9b35a
Thibault PLET <thibault.plet@gmail.com>
2016-08-18 12:22:42 +0200

    Create polyfill for delay and millis method

---
+++ com/osteres/automation/polyfill/millis.h
@@ -0,0 +1,19 @@
+//
+// Created by Thibault PLET on 18/08/2016.
+//
+
+#ifndef COM_OSTERES_AUTOMATION_POLYFILL_MILLIS_H
+#define COM_OSTERES_AUTOMATION_POLYFILL_MILLIS_H
+
+// Substitute for millis() method
+#ifndef ARDUINO
+
+    #include <ctime>
+    unsigned long millis()
+    {
+        return clock() / (double)CLOCKS_PER_SEC * 1000;
+    }
+
+#endif
+
+#endif //COM_OSTERES_AUTOMATION_POLYFILL_MILLIS_H
